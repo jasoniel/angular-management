84a8cc72b4738cea57b7b7a0f42a802e1a688666
Jasoniel B Santana <jasoniel@MacBook-Air-de-Jasoniel.local>
2019-03-17 20:31:47 -0300

    Project Finish

---
+++ src/app/price-display/price-display.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit, Input } from "@angular/core";
+
+@Component({
+  selector: "price-display",
+  templateUrl: "./price-display.component.html",
+  styleUrls: ["./price-display.component.css"]
+})
+export class PriceDisplayComponent implements OnInit {
+  @Input() price: number;
+
+  constructor() {}
+
+  ngOnInit() {}
+}
